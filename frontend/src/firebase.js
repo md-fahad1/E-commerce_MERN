3a78bb7dffc36824303383142654adf2387421ca
md-fahad1 <20-43328-1@student.aiub.edu>
2025-09-09 19:29:22 +0600

    update code for deploy

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "mern-ecommerce-66fc4.firebaseapp.com",
   projectId: "mern-ecommerce-66fc4",
-  storageBucket: "mern-ecommerce-66fc4.appspot.com",
+  storageBucket: "mern-ecommerce-66fc4.firebasestorage.app",
   messagingSenderId: "361095495781",
   appId: "1:361095495781:web:0e9363fad7e95180d9468c",
 };
